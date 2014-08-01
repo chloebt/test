44ecfe5c87d4e2c131f241837b6622968be10ead
Chloe Braud <chloe.braud@gmail.com>
2014-08-01 11:43:05 +0200

    Modifying main file

--- main.py
+++ main.py
@@ -2,5 +2,12 @@
 
 import codecs, re, sys, os
 
+def main():
+    import argparse
+
+    stream = codecs.open(sys.argv[1], 'r', 'utf-8')
+    for line in stream:
+        print line.strip()
+
 if __name__ == '__main__':
     main()
